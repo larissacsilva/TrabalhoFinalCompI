f39e7fd352dd5384d8d3fed6a4071d8630fd5b55
Larissa do Carmo Silva <05924001156@seguranca.local>
2021-09-30 12:52:52 -0400

    Adicionando Arquivo

---
+++ analisadorLexico.py
@@ -0,0 +1,349 @@
+'''
+Aluna: Larissa do Carmo Silva           
+RGA: 20171131040
+
+'''
+import sys
+
+class Token():
+  def __init__(self, tipo, termo):
+    self.tipo = tipo
+    self.termo = termo
+
+class AnalisadorLexico () :
+  RESERVADAS = ["var", "integer", "real", "if", "then", "while", "do", "write", "read", "begin", "end", "program"]
+
+  def __init__(self):
+    self.conteudo = []
+    self.table = []
+    self.buffer = []
+    self.estado = 0
+    self.pos = 0
+    self.linha = 1
+    self.atual = ''
+    self.erro = False
+    self.token = None
+    self.completo = False
+
+  def abrirArquivo (self, filename) :
+    with open(filename) as file:
+      self.conteudo = file.read()
+    self.conteudo += ' '
+
+  def fecharArquivo(self):
+    return self.pos == len(self.conteudo)
+
+
+  def espaco(self, char):
+    if(char in ['', ' ', '\n', '\t', '\r'] ):
+       if(char=='\n'):
+         self.linha += 1
+       return True
+    return False
+
+  
+  def digito(self, char):
+    if(char in ['0','1','2','3','4','5','6','7','8','9']):
+      self.buffer.append(char)
+      return True
+    return False 
+
+  def simbolo(self, char):
+    if(char in ['(',')','+','-','*','/',',',';',':','.','>','<','=']):
+      self.buffer.append(char)
+      return True
+    return False 
+
+  def alpha(self, char):
+    if( char >= 'A' and char <= 'Z' or  char >= 'a' and char <= 'z'):
+      self.buffer.append(char)
+      return True
+    return False
+
+
+  def apenasAlpha(self, char):
+    return char.isalpha()
+  
+  def apenasNum(self, char):
+     return char.isdigit() 
+
+  def apenasSimbolo(self, char):
+     return char in '()+-*/.;:<>='
+      
+      
+
+  def proximoChar(self):
+    if( self.fecharArquivo()):
+      self.completo = True
+      return ' '
+
+    aux = self.conteudo[self.pos]
+    
+    self.pos += 1
+
+    return aux
+
+  def back(self):
+    self.pos -= 1
+    self.buffer = []
+    return self.conteudo[self.pos]
+
+  def print_table(self):
+    for i in self.table:
+      print(i)
+
+  def _PegarToken(self):
+    return [self.table]
+      
+
+  def analyse(self):
+    self.abrirArquivo('exemplo.txt')
+    print("\n - Rodando -... ")
+
+    while True : 
+      self.proximoSimbolo()
+      if (self.token == None):
+        break
+
+
+    print("\n ... - end - ")
+
+
+
+  def proximoSimbolo(self):
+
+    self.estado = 0
+    self.token = None
+
+    while( not self.completo):
+
+      if(self.estado == 0):
+        self.estado0()
+
+      elif(self.estado == 1):
+        self.estado1()
+
+      elif(self.estado == 2):
+        self.estado2()
+
+      elif(self.estado == 3):
+        self.estado3()
+      
+      elif(self.estado == 4):
+        self.estado4()
+
+      elif(self.estado == 5):
+        self.estado5()
+      
+      elif(self.estado == 6):
+        self.estado6()
+
+      elif(self.estado == 7):
+        self.estado7()
+      
+      elif(self.estado == 8):
+        self.estado8()
+
+      elif(self.estado == 9):
+        self.estado9()
+
+      elif(self.estado == 10):
+        self.estado10()
+
+      elif(self.estado == 11):
+        self.estado11()
+
+      elif(self.estado == 12):
+        self.estado12()    
+      
+      elif(self.estado == 13):
+        self.estado13()  
+      
+  # Estados impl 
+
+  def estado0 (self):
+    if(self.espaco(self.atual)):
+      self.estado = 0
+      self.atual = self.proximoChar()
+
+    elif(self.digito(self.atual)):
+      self.estado = 1
+      self.atual = self.proximoChar()
+
+    elif(self.alpha(self.atual)):
+      self.estado = 6
+      self.atual = self.proximoChar()
+
+    elif(self.atual in ['(',')','+','-','*','/',',',';','.','$','=']):
+      self.buffer.append(self.atual)
+      self.estado = 8
+      self.atual = self.proximoChar()
+
+    elif(self.atual in [':', '>']):
+      self.buffer.append(self.atual)
+      self.estado = 10
+      self.atual = self.proximoChar()
+
+    elif(self.atual == '<'):
+      self.buffer.append(self.atual)
+      self.estado = 12
+      self.atual = self.proximoChar()
+
+    else :
+      print(f'\n Erro:  estado 0 - char {self.atual}\n')
+      self.erro = True
+      sys.exit()
+
+
+  def estado1(self):
+
+    if(self.digito(self.atual)):
+      self.estado = 1
+      self.atual = self.proximoChar()
+    elif(self.atual == '.'):
+      self.buffer.append(self.atual)
+      self.estado = 3
+      self.atual = self.proximoChar()
+    elif (self.apenasAlpha(self.atual) or self.espaco(self.atual) or self.apenasSimbolo(self.atual)):
+      self.estado = 2
+    else : 
+      print(f'\n Erro:  estado 1 - char {self.atual}\n')
+      self.erro = True
+      sys.exit()
+      
+
+  def estado2(self): 
+    termo = ''.join(self.buffer)
+    self.token =  Token(termo=termo, tipo='INTEGER')
+    self.table.append(['num_integer', termo])
+    self.buffer = []
+
+    self.estado = 0
+
+
+
+  def estado3(self):
+    if(self.digito(self.atual)):
+      self.estado = 4
+      self.atual = self.proximoChar()
+    else : 
+      print(f'\n Erro:  estado 3 - char {self.atual}\n')
+      self.erro = True
+      sys.exit()
+
+    
+  def estado4(self):
+    if(self.digito(self.atual)):
+      self.estado = 4
+      self.atual = self.proximoChar()
+    elif (self.alpha(self.atual) or self.espaco(self.atual) or self.simbolo(self.atual)):
+      self.estado = 5
+      
+    else : 
+      print(f'\n Erro:  estado 4 - char {self.atual}\n')
+      self.erro = True
+      sys.exit()
+
+
+  def estado5(self):
+    termo = ''.join(self.buffer)
+    self.token = Token(termo=termo, tipo='REAL')
+    self.table.append(['num_real', termo])
+    self.buffer = []
+
+    self.estado = 0 
+    self.atual = self.proximoChar()
+
+
+  
+  def estado6(self):  
+    if (self.alpha(self.atual) or self.digito(self.atual)):
+      self.estado = 6
+      self.atual = self.proximoChar()
+    elif(self.espaco(self.atual) or self.atual in ['(',')','+','-','*','/',',',';',':','.','>','<','=']):
+      self.estado = 7
+
+    else : 
+      print(f'\n Erro:  estado 6 - char {self.atual}\n')
+      self.erro = True
+      sys.exit()
+
+
+  def estado7(self):
+    termo = ''.join(self.buffer)
+    tipo='IDENTIFICADOR'
+    if(termo in self.RESERVADAS):
+      tipo = 'RESERVADA'
+      self.table.append(['reserv', termo])
+    else:
+       self.table.append(['ident', termo])
+    self.token = Token(termo=termo, tipo=tipo)
+    
+    self.buffer = []
+    self.estado = 0 
+    
+
+    
+  def estado8(self):
+    self.estado = 9
+
+  def estado9(self):
+    termo = ''.join(self.buffer)
+    self.token  = Token(termo=termo, tipo='SYMBOL')
+    self.table.append(['symbol', termo])
+    self.buffer = []
+    self.estado = 0 
+    
+
+
+  def estado10(self):
+    if (self.alpha(self.atual) or self.espaco(self.atual) or self.digito(self.atual)):
+      self.estado = 9
+      self.atual = self.proximoChar()
+    elif(self.atual == '='):
+      self.buffer.append(self.atual)
+      self.estado = 11
+      self.atual = self.proximoChar()
+    else : 
+      print(f'\n Erro: estado 10 - char {self.atual}\n')
+      self.erro = True
+      sys.exit()
+
+
+  def estado11(self):
+    if (self.alpha(self.atual) or self.espaco(self.atual) or self.simbolo(self.atual)):
+      self.estado = 9
+      self.atual = self.proximoChar()
+    else : 
+      print(f'\n Erro: estado 11 - char {self.atual}\n')
+      self.erro = True
+      sys.exit()
+
+
+  def estado12(self):
+    if (self.alpha(self.atual) or self.espaco(self.atual) or self.digito(self.atual)):
+      self.estado = 9
+      self.atual = self.proximoChar()
+    elif(self.atual in ['=', '>']):
+      self.buffer.append(self.atual)
+      self.estado = 13
+      self.atual = self.proximoChar()
+    else : 
+      print(f'\n Erro: estado 12 - char {self.atual}\n')
+      self.erro = True
+      sys.exit()
+
+  def estado13(self): 
+    if (self.alpha(self.atual) or self.espaco(self.atual) or self.digito(self.atual)):
+      self.estado = 9
+      self.atual = self.proximoChar()
+    else : 
+      print(f'\n Erro: estado 13 - char {self.atual}\n')
+      self.erro = True
+      sys.exit()
+
+
+
+# anLex = AnalisadorLexico()
+# anLex.analyse()
+# anLex.print_table()
---
+++ analisadorSintatico.py
@@ -0,0 +1,406 @@
+'''
+Aluna: Larissa do Carmo Silva           
+RGA: 20171131040
+
+'''
+
+import sys
+
+from analisadorLexico import AnalisadorLexico
+
+class AnalisadorSintatico():
+
+  def __init__(self):
+    self.currentLine = 0
+    self.currentLexema = 0
+    self._allTokens = []
+    self.amountLines = 0
+    self.amountLexema = 0
+    self.tokens = [[]]
+
+  def setTokens(self, generatedTokens):
+    self._allTokens = generatedTokens
+    self.amountLines = len(self._allTokens)
+    self.amountLexema = sum([ len(lineTokens) for lineTokens in self._allTokens])
+    self.tokens = self._allTokens
+
+
+  def _isEmptyLine(self, line):
+    return len(line) == 0
+
+  def _nextToken(self):
+    
+    if(self.currentLexema <= self.amountLexema):       
+      self.currentLexema += 1
+
+      print(self.tokens[self.currentLexema])
+    else :
+      print(" *** [terminatedtokens] *** ")
+  
+  def _currentToken(self):
+
+    if(len(self.tokens) > 0):
+      return self.tokens[self.currentLexema]
+    
+    return []
+
+  def _getChaveToken(self):
+    return (self._currentToken()[0] if len(self._currentToken())> 0 else 'erroReadKey')
+
+  def _getValorToken(self):
+    return  (self._currentToken()[1] if len(self._currentToken())> 0 else 'erroReadToken')
+
+
+# --- --- --- --- Analisador Sintatico --- --- --- ---
+
+  def analyse(self):
+    print("\n Rodando [Analisador Sintatico] ------------ \n")
+    self._Programa ()
+  
+
+  def _Programa(self):
+    if(self._getValorToken() == 'program'):
+      self._nextToken()
+    
+      if(self._getChaveToken() == 'ident'):
+        self._nextToken()
+        self._Corpo()
+        if(self._getValorToken() == '.'):
+            self._SuccessCode()
+        else:
+            print("[Programa]- Era esperado um '.'")
+            self._Erro()  
+      else:
+        print("[Programa]- Era esperado um identificador válido ")
+        self._Erro()
+    else :      
+      print("[Programa]- Era esperado program ")
+      self._Erro()
+
+
+  def _Corpo(self):
+    self._Dc()
+
+    if(self._getValorToken() == 'begin'):
+      self._nextToken()
+      self._Comandos()
+          
+      if(self._getValorToken() == 'end'):
+        self._nextToken()
+      else:
+        print("[Corpo]- Era esperado 'end' " )
+        self._Erro()
+    else:
+      print("[Corpo]- Era esperado 'begin' ")
+      self._Erro()
+
+
+  def _Dc(self):
+    if(self._getValorToken() in ['real', 'integer']):
+      self._Dc_v()
+      self._Mais_dc()
+    else:
+      self._Vazio()
+
+
+  def _Mais_dc(self):
+    if(self._getValorToken() == ';'):
+      self._nextToken()
+      self._Dc()
+    else:
+      self._Vazio()
+    
+
+  def _Dc_v(self):
+    self._Tipo_var()
+          
+    if(self._getValorToken() == ':'):
+        self._nextToken()
+        self._Variaveis()
+    else:
+        print("[Dc_v]- Era esperado ':' " )
+        self._Erro()
+    
+
+  def _Tipo_var(self):
+    if(self._getValorToken() in ['real', 'integer']):
+      self._nextToken()
+    else:
+      print("[Tipo_var] - Era esperado 'real' ou 'integer'")
+      self._Erro()
+
+
+  def _Variaveis(self):
+    if(self._getChaveToken() == 'ident'):
+      self._nextToken()
+      self._Mais_var()
+    else:
+      print("[Variaveis] - Era esperado 'identificador' ")
+      self._Erro()
+
+
+  def _Mais_var(self):   
+    if(self._getValorToken() == ','):
+      self._nextToken()
+      self._Variaveis()
+    else:
+      self._Vazio()
+
+
+  def _PFalsa(self):
+    if(self._getValorToken() == 'else'):
+      self._nextToken()
+      self._Comandos()
+    else:
+      self._Vazio()
+
+
+  def _Comandos(self):
+    self._Comando()
+    self._Mais_comandos()
+
+
+  def _Mais_comandos(self):
+    if(self._getValorToken() == ';'):
+      self._nextToken()
+      self._Comandos()
+    else: 
+      self._Vazio()
+
+
+  def _Comando(self):
+    itemKey = self._getChaveToken()
+    itemValue = self._getValorToken()
+    if(itemValue == 'read' or itemValue == 'write' ):
+      self._nextToken()
+      
+      if(self._getValorToken() == '('):
+        self._nextToken()
+
+        if(self._getChaveToken() == 'ident'):
+          self._nextToken()
+          if(self._getValorToken() == ')'):
+            self._nextToken()
+          else :
+            print("[Comando]- Era esperado ')'")
+            self._Erro()
+        else:
+          print("[Variaveis] - Era esperado 'identificador' ")
+          self._Erro()
+
+      else:
+       print("[Comando]- Era esperado '('")
+       self._Erro()
+
+    elif(self._getChaveToken() == 'ident'):
+      self._nextToken()
+      if(self._getValorToken() == ':='):
+        self._nextToken()
+        self._Expressao()
+      else:
+        print("[Comando] - Era esperado ':=' ")
+        self._Erro()  
+
+    elif(itemValue == 'if'):
+      self._nextToken()
+      self._Condicao()
+
+      if(self._getValorToken() == 'then'):
+        self._nextToken()
+        self._Comandos()
+        self._PFalsa()
+        
+        if(self._getValorToken() == '$'):
+          self._nextToken()
+        else :
+          print("[Comando]- Era esperado '$'")
+          self._Erro()
+      else:
+        print("[Comando]- Era esperado 'then'")
+        self._Erro()
+
+    else:
+      print("[Comando]- Era esperado 'comando' ou 'identificador'")
+      self._Erro()
+
+
+
+  def _Condicao(self):
+    self._Expressao()
+    self._Relacao()
+    self._Expressao()
+
+
+  def _Relacao(self):
+    if(self._getValorToken() in ["=",">","<",">=","<=","<>"]):
+      self._nextToken()
+    else:
+      print("[Relacao]- Era esperado 'simbolo de relacao'")
+      self._Erro()
+
+
+  def _Expressao(self):
+    self._Termo()
+    self._Outros_termos()
+
+
+  def _Op_un(self):
+    if(self._getValorToken() in ['-']):
+      self._nextToken()
+    else:
+      self._Vazio()
+
+
+  def _Outros_termos(self):
+    if(self._getValorToken() in ['+', '-']):
+      self._Op_ad()
+      self._Termo()
+      self._Outros_termos()
+    else :
+      self._Vazio()
+
+
+  def _Op_ad(self):
+    if(self._getValorToken() in ['+', '-']):
+      self._nextToken()
+    else:
+      print("[Op_ad]- Era esperado '+' ou '-'")
+      self._Erro()
+
+
+  def _Termo(self):
+    self._Op_un()
+    self._Fator()
+    self._Mais_fatores()
+
+
+  def _Mais_fatores(self):
+    if(self._getValorToken() in ['*','/']):
+      self._Op_mul()
+      self._Fator()
+      self._Mais_fatores()
+    else:
+      self._Vazio()
+
+
+  def _Op_mul(self):
+    if(self._getValorToken() in ['*','/']):
+      self._nextToken()
+    else:
+      print("[Op_mul]- Era esperado '*' ou '/'")
+      self._Erro()
+
+
+
+  def _Fator(self):
+    if(self._getValorToken()=="("):
+      self._nextToken()
+      self._Expressao()
+      
+      if(self._getValorToken() ==")"):
+        self._nextToken()
+      else : 
+        print("Era esperado ')'")   
+        self._Erro()
+    elif(self._getChaveToken() in ['ident','num_integer', 'num_real']):
+      self._nextToken()
+      
+    else:
+      print("[Fator]- Era esperado '(', 'identificador' ou 'numero'")
+      self._Erro()
+
+
+  def _Vazio (self) :
+    pass
+
+  def _Erro (self):
+    print('\nEncerrando programa... \n')
+    sys.exit()
+
+  def _SuccessCode(self):
+    print('[SyntacticAnalyzer] -End \n' )
+
+test = [
+['reserv', 'program'],
+['ident', 'teste'],
+['reserv', 'real'],
+['symbol', ':'],
+['ident', 'a'] ,
+['symbol', ','],
+['ident', 'b'] ,
+['symbol', ';'],
+['reserv', 'integer'],
+['symbol', ':'],
+['ident', 'c'] ,
+['symbol', ','],
+['ident', 'd'] ,
+['symbol', ';'],
+['reserv', 'begin'],
+['reserv', 'read'],
+['symbol', '('],
+['ident', 'a'],
+['symbol', ')'],
+['symbol', ';'],
+['reserv', 'read'],
+['symbol', '('],
+['ident', 'c'],
+['symbol', ')'],
+['symbol', ';'],
+['ident', 'b'],
+['symbol', ':='],
+['ident', 'a'],
+['symbol', '*'],
+['ident', 'a'],
+['symbol', '+'],
+['ident', 'a'],
+['symbol', ';'],
+['ident', 'd'],
+['symbol', ':='],
+['num_integer', '2'],
+['symbol', '*'],
+['ident', 'c'],
+['symbol', '+'],
+['ident', 'c'],
+['symbol', ';'],
+['reserv', 'if'],
+['ident', 'a'],
+['symbol', '>'],
+['ident', 'b'],
+['reserv', 'then'],
+['reserv', 'write'],
+['symbol', '('],
+['ident', 'a'],
+['symbol', ')'],
+['ident', 'else'],
+['reserv', 'write'],
+['symbol', '('],
+['ident', 'b'],
+['symbol', ')'],
+['symbol', '$'],
+['symbol', ';'],
+['reserv', 'if'],
+['ident', 'd'],
+['symbol', '<'],
+['ident', 'c'],
+['reserv', 'then'],
+['reserv', 'write'],
+['symbol', '('],
+['ident', 'd'],
+['symbol', ')'],
+['ident', 'else'],
+['reserv', 'write'],
+['symbol', '('],
+['ident', 'c'],
+['symbol', ')'],
+['symbol', '$'],
+['reserv', 'end'],
+['symbol', '.'],
+]
+
+
+lex = AnalisadorLexico()
+lex.analyse()
+tokens = lex._PegarToken()
+sint = AnalisadorSintatico()
+sint.setTokens(test)
+sint.analyse()
